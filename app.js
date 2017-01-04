bc66fc9a3ecae3b84d9aedcc164ef9992a5f0ab8
Michael Saul <michael.saul@gmail.com>
2017-01-04 00:07:42 -0800

    Updated app.js with env vars.

--- app.js
+++ app.js
@@ -2,20 +2,24 @@
 var restify = require('restify');
 var builder = require('builder');
 
+// Get environment variables
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appPassword = process.env.MY_APP_PASSWORD || "Missing your app Password";
+
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function())
+server.listen(process.env.PORT || 3000, function()
 {
     console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppID', appPassword: 'YourAppPassword' });
+({ appId: appId, appPassword: appPassword });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Create bot dialoges
 bot.dialog('/', function (session) {
-    session.send("Hello World");
+    session.send("Aloha, you look like you need a drink.");
 });
